606655bc106f3b378a7c2c5c3ce424da0844252b
MatthiasssT - uni lab 2 lvl1 <1031235@student.sae.edu.au>
2023-09-26 11:11:58 +1000

    i fixed name

--- Assets/Scripts/WalkEffect.cs
+++ Assets/Scripts/WalkEffect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class walkeffect : MonoBehaviour
+public class WalkEffect : MonoBehaviour
 {
     [SerializeField] private bool isThiscool = true;
    
