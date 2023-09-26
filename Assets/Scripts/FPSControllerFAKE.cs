2942bb6d6f5411a4cd12ee41cc4194822e6eabe0
JamesCass - Lab 2 102 <136556196+JamesCass@users.noreply.github.com>
2023-09-26 10:28:14 +1000

    Created FPSControllerFAKE
    Created a fake movement controller script.

---
+++ Assets/Scripts/FPSControllerFAKE.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSControllerFAKE : MonoBehaviour
+{
+    //Fake FPS controller here
+    //Player moves on the X,Y and Z axis via assigned movement keys
+    //code complete.
+ 
+}
